52047a11eb47e1fe3d3956dd780f4838d5f12559
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-05-12 21:24:00 +0200

    Implemented Password Repo

--- src/main/kotlin/com/xander/passwordmanager/repository/PasswordRepositoryImpl.kt
+++ src/main/kotlin/com/xander/passwordmanager/repository/PasswordRepositoryImpl.kt
@@ -1,11 +1,10 @@
 package com.xander.passwordmanager.repository
 
-import com.xander.passwordmanager.model.PasswordEntry
+import com.xander.passwordmanager.persistence.PasswordEntry
 import com.xander.passwordmanager.persistence.PasswordEntries
 import com.xander.passwordmanager.security.PasswordEncryptor
 import org.ktorm.database.Database
 import org.ktorm.dsl.*
-import org.ktorm.entity.sequenceOf
 
 class PasswordRepositoryImpl(
     private val database: Database,
@@ -15,17 +14,31 @@ class PasswordRepositoryImpl(
         private val masterPassword: CharArray = "MasterPassword".toCharArray()
     }
 
-    private val passwordTable = database.sequenceOf(PasswordEntries)
-
     override fun addPassword(username: String, password: String, platform: String) {
-        TODO("Not yet implemented")
+        val encryptedPassword = PasswordEncryptor.encryptPassword(masterPassword, password).toString()
+
+        database.insertAndGenerateKey(PasswordEntries) {
+            set(it.username, username)
+            set(it.password, encryptedPassword)
+            set(it.platform, platform)
+        }
     }
 
     override fun getPassword(platform: String, username: String): String? {
-        val query = database.from(PasswordEntries)
+        val entryFromDb: PasswordEntry? = database.from(PasswordEntries)
             .select()
             .where{ (PasswordEntries.username eq username) and ( PasswordEntries.platform eq platform ) }
+            .limit(1)
+            .map {
+                PasswordEntry(
+                    id = it[PasswordEntries.id]!!.toLong(),
+                    username = it[PasswordEntries.username]!!.toString(),
+                    password = it[PasswordEntries.password]!!.toString(),
+                    platform = it[PasswordEntries.platform]!!.toString()
+                )
+            }
+            .firstOrNull()
 
-        query.map { row -> PasswordEntry(row[PasswordEntries.id], ) }
+        return if( entryFromDb != null ) PasswordEncryptor.decryptPassword(masterPassword, entryFromDb.password.toByteArray()) else null
     }
 }
