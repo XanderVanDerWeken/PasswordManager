78d6841453585fde86680e7e22d3cc1c6b6dcaaf
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-05-13 10:26:40 +0200

    001_Start
    - fixed Database
    - added init script

---
+++ src/main/kotlin/com/xander/passwordmanager/persistence/SqliteDatabase.kt
@@ -0,0 +1,46 @@
+package com.xander.passwordmanager.persistence
+
+import org.ktorm.database.Database
+import org.ktorm.support.sqlite.SQLiteDialect
+import org.sqlite.SQLiteDataSource
+import java.io.File
+
+object SqliteDatabase {
+    const val INIT_SCRIPT = "init-sqlite-data.sql"
+
+    private val dataSource = SQLiteDataSource().apply {
+        url = "jdbc:sqlite:${File("").absolutePath}/passwords.db"
+    }
+
+    val database = Database.connect(
+        dataSource = dataSource
+    )
+
+    init {
+        execSqlScript(INIT_SCRIPT, database)
+    }
+
+    /*fun connect() : Database {
+        return Database.connect (
+            url = "jdbc:sqlite::memory:",
+            dialect = SQLiteDialect()
+        )
+    }*/
+
+    fun execSqlScript(filename: String, database: Database) {
+        database.useConnection { conn ->
+            conn.createStatement().use { statement ->
+                javaClass.classLoader
+                    ?.getResourceAsStream(filename)
+                    ?.bufferedReader()
+                    ?.use { reader ->
+                        for( sql in reader.readText().split(";") ) {
+                            if(sql.any { it.isLetterOrDigit() }) {
+                                statement.executeUpdate(sql)
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
